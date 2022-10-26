10ece21ed464e37fc564d99a6a513faa6edf0c27
René Schleusner <schleusner@sitepark.com>
2022-10-26 17:00:45 +0200

    test: make TestLogger work nicely with PSR/Log 3.0

--- tests/TestLogger.php
+++ tests/TestLogger.php
@@ -10,39 +10,39 @@ class TestLogger implements \Psr\Log\LoggerInterface
 {
     public $content = "";
 
-    public function emergency($message, array $context = array()) {
+    public function emergency($message, array $context = array()): void {
         $this->content .= $message . PHP_EOL;
     }
 
-    public function alert($message, array $context = array()) {
+    public function alert($message, array $context = array()): void {
         $this->content .= $message . PHP_EOL;
     }
 
-    public function critical($message, array $context = array()) {
+    public function critical($message, array $context = array()): void {
         $this->content .= $message . PHP_EOL;
     }
 
-    public function error($message, array $context = array()) {
+    public function error($message, array $context = array()): void {
         $this->content .= $message . PHP_EOL;
     }
 
-    public function warning($message, array $context = array()) {
+    public function warning($message, array $context = array()): void {
         $this->content .= $message . PHP_EOL;
     }
 
-    public function notice($message, array $context = array()) {
+    public function notice($message, array $context = array()): void {
         $this->content .= $message . PHP_EOL;
     }
 
-    public function info($message, array $context = array()) {
+    public function info($message, array $context = array()): void {
         $this->content .= $message . PHP_EOL;
     }
 
-    public function debug($message, array $context = array()) {
+    public function debug($message, array $context = array()): void {
         $this->content .= $message . PHP_EOL;
     }
 
-    public function log($level, $message, array $context = array()) {
+    public function log($level, $message, array $context = array()): void {
         $this->content .= $message . PHP_EOL;
     }
 }
