a617b36c7e647aee13f9245f5fa5f0054c119833
René Schleusner <schleusner@sitepark.com>
2022-10-26 16:23:49 +0200

    fix: make logger option compatible with PSR-3 Logger

--- tests/TestLogger.php
+++ tests/TestLogger.php
@@ -6,17 +6,43 @@
 
 namespace DeepL;
 
-class TestLogger implements LoggerInterface
+class TestLogger implements \Psr\Log\LoggerInterface
 {
     public $content = "";
 
-    public function info(string $message, array $context = array())
-    {
+    public function emergency($message, array $context = array()) {
         $this->content .= $message . PHP_EOL;
     }
 
-    public function debug(string $message, array $context = array())
-    {
+    public function alert($message, array $context = array()) {
+        $this->content .= $message . PHP_EOL;
+    }
+
+    public function critical($message, array $context = array()) {
+        $this->content .= $message . PHP_EOL;
+    }
+
+    public function error($message, array $context = array()) {
+        $this->content .= $message . PHP_EOL;
+    }
+
+    public function warning($message, array $context = array()) {
+        $this->content .= $message . PHP_EOL;
+    }
+
+    public function notice($message, array $context = array()) {
+        $this->content .= $message . PHP_EOL;
+    }
+
+    public function info($message, array $context = array()) {
+        $this->content .= $message . PHP_EOL;
+    }
+
+    public function debug($message, array $context = array()) {
+        $this->content .= $message . PHP_EOL;
+    }
+
+    public function log($level, $message, array $context = array()) {
         $this->content .= $message . PHP_EOL;
     }
 }
