1360a7ae8f43fa832ed2559e4272b96eaf2e20c3
Daniel Jones <daniel.jones@deepl.com>
2022-12-01 16:33:01 +0100

    refactor: code sniffer fixes

--- tests/TestLogger.php
+++ tests/TestLogger.php
@@ -10,39 +10,48 @@ class TestLogger implements \Psr\Log\LoggerInterface
 {
     public $content = "";
 
-    public function emergency($message, array $context = array()): void {
+    public function emergency($message, array $context = array()): void
+    {
         $this->content .= $message . PHP_EOL;
     }
 
-    public function alert($message, array $context = array()): void {
+    public function alert($message, array $context = array()): void
+    {
         $this->content .= $message . PHP_EOL;
     }
 
-    public function critical($message, array $context = array()): void {
+    public function critical($message, array $context = array()): void
+    {
         $this->content .= $message . PHP_EOL;
     }
 
-    public function error($message, array $context = array()): void {
+    public function error($message, array $context = array()): void
+    {
         $this->content .= $message . PHP_EOL;
     }
 
-    public function warning($message, array $context = array()): void {
+    public function warning($message, array $context = array()): void
+    {
         $this->content .= $message . PHP_EOL;
     }
 
-    public function notice($message, array $context = array()): void {
+    public function notice($message, array $context = array()): void
+    {
         $this->content .= $message . PHP_EOL;
     }
 
-    public function info($message, array $context = array()): void {
+    public function info($message, array $context = array()): void
+    {
         $this->content .= $message . PHP_EOL;
     }
 
-    public function debug($message, array $context = array()): void {
+    public function debug($message, array $context = array()): void
+    {
         $this->content .= $message . PHP_EOL;
     }
 
-    public function log($level, $message, array $context = array()): void {
+    public function log($level, $message, array $context = array()): void
+    {
         $this->content .= $message . PHP_EOL;
     }
 }
