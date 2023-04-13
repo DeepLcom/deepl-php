0353cf68cf091e6cd1cd97fe55ff20092d23e92c
Jan Ebbing <Jan.Ebbing@deepl.com>
2023-04-13 11:53:12 +0100

    fix: add copyright header to InvalidContentException.php

--- src/InvalidContentException.php
+++ src/InvalidContentException.php
@@ -1,5 +1,9 @@
 <?php
 
+// Copyright 2022 DeepL SE (https://www.deepl.com)
+// Use of this source code is governed by an MIT
+// license that can be found in the LICENSE file.
+
 namespace DeepL;
 
 use JsonException;
